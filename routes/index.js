4c9f75ca070e96ff8adc326d000a178a9fb8da34
zhvirblis <yurocker@mail.ru>
2016-04-08 16:03:26 +0300

    Реализация постов с комментариями >10

--- routes/index.js
+++ routes/index.js
@@ -9,48 +9,45 @@ var footer="&copy GRAB13";
 var request = require("request");
 var cheerio = require("cheerio");
 var link = "http://s13.ru/";
-/*exports.index = function(req, res){
-  var urlParsed = url.parse(req.url, true);
-  var numberpage = 1;
-  if(urlParsed.query.page) numberpage = urlParsed.query.page;
-  request(link, function (error, response, body) {
-    	if (!error) {
-        	var $ = cheerio.load(body);
-        	var posts="";
-            $("a").each(function(i, elem){
-              var hrefs=$(this).attr("href").split('/');
-              $(this).attr("href","/"+hrefs[hrefs.length-2]+"/"+hrefs[hrefs.length-1]);
-            });
-            $(".item").each(function(i, elem){
-
-            	posts +=$(this).html();
-            });
-
-            res.render('index', { title:"Домашняя", sitename:sitename, footer:footer, content:posts});
-    	} else {
-        	
-    	}
-    });
-}*/
 exports.archive = function(req, res){
     var urlParsed = url.parse(req.url, true);
     //var pPath=urlParsed.pathname.split('/');
    // if(pPath[1]==="archives"){
     //res.render('index', { title:"Архив", sitename:sitename, footer:footer, content:"archive:"+pPath[2]});
-      request(link+urlParsed.pathname, function (error, response, body) {
+
+      /*request(link+urlParsed.pathname, function (error, response, body) {
       if (!error) {
           var $ = cheerio.load(body);
           var posts="";
           $("a").each(function(i, elem){
               var hrefs=$(this).attr("href").split('/');
               $(this).attr("href","/"+hrefs[hrefs.length-2]+"/"+hrefs[hrefs.length-1]);
-              console.log(link);
             });
           $(".primary").each(function(i, elem){
               posts +=$(this).html();
             });
-          var title = $("title").html();
+          var title = $("title").text();
+          console.log(posts);
+          res.render('index', { title:title, sitename:sitename, footer:footer, content:posts});
+      }else {
           
+      }
+    });*/
+    request(link+urlParsed.pathname, function (error, response, body) {
+      if (!error) {
+          var $ = cheerio.load(body);
+          var posts="";
+          $("a").each(function(i, elem){
+              var hrefs=$(this).attr("href").split('/');
+              $(this).attr("href","/"+hrefs[hrefs.length-2]+"/"+hrefs[hrefs.length-1]);
+            });
+          $("a.commentslink").each(function(i, elem){
+            if($(elem).text()>10){
+              posts +=$(elem).parent().parent().parent().html();
+            }
+            });
+          var title = $("title").text();
+          console.log(posts);
           res.render('index', { title:title, sitename:sitename, footer:footer, content:posts});
       }else {
           
